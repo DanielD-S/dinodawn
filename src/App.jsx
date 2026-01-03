fc169f7f6769f3d592f4ad7149622ec48c407f8e
DanielD-S <daniel.diazsantander@gmail.com>
2026-01-03 19:40:29 -0300

    init: Dino Dawn MVP v0.1 base funcionando

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Dino Dawn – MVP v0.1</h1>
+      <p>Si ves esto, React está funcionando.</p>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Dino Dawn – MVP v0.1</h1>
+    </div>
+  )
+}
+
+export default App
